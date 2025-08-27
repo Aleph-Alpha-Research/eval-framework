a9818132100adbb7b7e2416fb356fa94af3e7b78
Dylan Rodriquez <dylan.rodriquez@aleph-alpha-ip.ai>
2025-08-27 10:21:46 +0000

    reworking how artifacts are consumed

--- src/eval_framework/utils/file_ops.py
+++ src/eval_framework/utils/file_ops.py
@@ -1,5 +1,6 @@
+from enum import Enum
 from pathlib import Path
-from typing import Tuple
+from typing import List, Tuple
 import boto3
 import wandb
 import urllib
@@ -7,6 +8,9 @@ import tempfile
 import os
 from concurrent.futures import ThreadPoolExecutor
 
+class FileSystem(Enum):
+    LOCAL = "local"
+    S3 = "s3"
 
 class WandbFs:
     def __init__(self):
@@ -37,6 +41,20 @@ class WandbFs:
     def entity(self):
         return self.api.entity
 
+    def create_file_tree(self, artifact_reference_list: List[str]) -> dict:
+        tree = {"files": []}
+        for file in artifact_reference_list:
+            dirs = file.strip(r"s3://").split(r"/")
+            c = tree
+            for _, d in enumerate(dirs):
+                if "." in d:
+                    c["files"].append(d)
+                else:
+                    if d not in c:
+                        c[d] = {"files": []}
+                    c = c[d]
+        return tree
+
     def get_artifact(self, artifact_id: str, version: str="latest"):
         return self.api.artifact(f"wandb-registry-model/{artifact_id}:{version}")
 
@@ -48,16 +66,24 @@ class WandbFs:
         file_list = list(map(lambda x: x.path_uri, [x for x in artifact.files()]))
         return file_list
         
+    def file_system_detector(self, file_list: List[str]):
+        if all(file.startswith("s3://") for file in file_list):
+            return FileSystem.S3
+        return FileSystem.LOCAL
 
-    def download_artifacts(self, artifact: wandb.Artifact):
+    def download_artifacts(self, artifact: wandb.Artifact) -> Path:
+        """download_artifacts downloads all artifacts associated with a registered artifact
+
+        If the filesystem is local, then the artifact references the path of the checkpoint directory. 
+        """
         file_list = self.ls(artifact)
         with ThreadPoolExecutor() as executor:
             executor.map(self._download_artifact, file_list[:4])
+        return self.temp_dir.name
 
     def _download_artifact(self, file):
         local_path = Path(urllib.parse.urlparse(file).path)
         bucket, prefix = self.get_bucket_prefix(file)
-
         # create and write to tempdir
         if self.temp_dir is None:
             self.temp_dir = tempfile.TemporaryDirectory()
@@ -67,6 +93,23 @@ class WandbFs:
         with open(local_temp_path, "wb") as f:
             self.s3_client.download_fileobj(Bucket=bucket, Key=prefix.lstrip('/'), Fileobj=f)
 
+
+    def use_artifact(self, artifact):
+        """
+        use_artifact determines the filesystem that the artifact is on, and does one of two things:
+        1. if the file system is remote (s3 type storage) then it will download the artifact to a temp diectory
+           we then call wandb.use_artifact on the artifact that we downloaded.
+
+        2. if the file system is local, then we just call wandb.use_artifact
+        """
+        file_system = self.file_system_detector(self.ls(artifact))
+        if file_system == FileSystem.LOCAL:
+            pass
+        elif file_system == FileSystem.S3:
+            artifact_dir = self.download_artifacts(artifact)
+        return artifact_dir
+
+
     def __enter__(self):
         return self
 
@@ -75,19 +118,7 @@ class WandbFs:
         if self.temp_dir:
             self.temp_dir.cleanup()
 
-def create_file_tree(artifact_reference_list):
-    tree = {"files": []}
-    for file in artifact_reference_list:
-        dirs = file.strip(r"s3://").split(r"/")
-        c = tree
-        for _, d in enumerate(dirs):
-            if "." in d:
-                c["files"].append(d)
-            else:
-                if d not in c:
-                    c[d] = {"files": []}
-                c = c[d]
-    return tree
+
     
 def rec(directory, current_path):
     if len(directory):
@@ -98,5 +129,7 @@ def rec(directory, current_path):
 
 if __name__ == "__main__":
     with WandbFs() as wandb_fs:
-        artifact = wandb_fs.get_artifact("qwen-3-32b-dccp", "latest")
+        name = "qwen-3-32b-dccp"
+        version = "latest"
+        artifact = wandb_fs.get_artifact(name, version)
         wandb_fs.download_artifacts(artifact)
