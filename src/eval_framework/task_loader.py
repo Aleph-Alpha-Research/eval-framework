fdcabf062ae85755379e79463f4ef88fa8eb71f7
GrS-AA <171042778+GrS-AA@users.noreply.github.com>
2025-08-29 13:00:29 +0000

    fix the duplicate task name issue in import of consumable tasks

--- src/eval_framework/task_loader.py
+++ src/eval_framework/task_loader.py
@@ -50,9 +50,15 @@ def load_extra_tasks(module_paths: Sequence[str]) -> None:
                 if not hasattr(obj, "NAME"):
                     logger.info(f"[User Task Loader] Skipping {obj.__module__} - no NAME attribute present.")
                 else:
-                    name_upper = obj.NAME.upper()
+                    # list the current registered task names to check for duplicates
+                    list_task_names_upper = [
+                        getattr(t.value, "NAME").upper() for t in TaskName if hasattr(t.value, "NAME")
+                    ]
+
                     # check if task name already exists (case-insensitive)
-                    if hasattr(TaskName, obj.NAME) or hasattr(TaskName, name_upper):
+                    name_upper = obj.NAME.upper()
+                    if name_upper in list_task_names_upper:
+                        # two classes with the same NAME attribute
                         logger.info(obj.__module__)
 
                         # if it comes from eval_framework's built-in tasks then will just skip (no need to register
@@ -61,6 +67,7 @@ def load_extra_tasks(module_paths: Sequence[str]) -> None:
                         if "eval_framework.tasks.benchmarks" not in obj.__module__:
                             # skip if import comes from eval_framework's built-in tasks
                             raise ValueError(f"Duplicate user task name found (case-insensitive): {obj.NAME}")
+
                     else:
                         # if there is no duplicate name conflict then register the new task
                         class_obj = getattr(user_module, name)
