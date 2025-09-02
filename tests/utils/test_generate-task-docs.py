c9da2560c828219baa8ce428caf289627f8a305e
GrS-AA <171042778+GrS-AA@users.noreply.github.com>
2025-09-02 11:45:18 +0000

    create a test for generate-task-docs.py

---
+++ tests/utils/test_generate-task-docs.py
@@ -0,0 +1,13 @@
+def test_task_docs_are_up_to_date(tmp_dir):
+    """
+    Test that all tasks docs have been generated and are up to date. If not they are updated locally and the test fails
+    on the CI until it gets commited. Checks that no documentation still remain from removed tasks.
+    """
+    raise NotImplementedError
+
+
+def test_generate_task_docs(tmp_dir):
+    """
+    Test that the task documentation generation script runs without errors on a dummy task class.
+    """
+    raise NotImplementedError
