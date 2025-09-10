763dee16e20c785051789be6952f8969ef961de7
GrS-AA <171042778+GrS-AA@users.noreply.github.com>
2025-09-10 16:04:28 +0000

    fix typo

--- src/eval_framework/utils/generate_task_docs.py
+++ src/eval_framework/utils/generate_task_docs.py
@@ -107,7 +107,7 @@ def generate_docs_for_task(
 
         f.write(f"- Module: [{task_class.__module__}]({task_class.__module__})\n\n")
         task_file = re.sub(".*eval_framework/", "src/eval_framework/", inspect.getfile(task_class))
-        task_file = re.sub(".*eval_framework_companion/", "src/eval_framework_companion/", inspect.getfile(task_class))
+        task_file = re.sub(".*eval_framework_companion/", "src/eval_framework_companion/", task_file)
         f.write(f"- File: [{task_file}](../../{task_file})\n\n")
 
         if http_path:
