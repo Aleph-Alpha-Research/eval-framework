a52c6b8994be5e43de2f774ea99d87d9626236cc
GrS-AA <171042778+GrS-AA@users.noreply.github.com>
2025-11-17 13:32:29 +0000

    add API imports

--- src/eval_framework/llm/models.py
+++ src/eval_framework/llm/models.py
@@ -6,6 +6,9 @@ to use it.
 
 from eval_framework.utils.packaging import is_extra_installed
 
+if is_extra_installed("api"):
+    from eval_framework.llm.aleph_alpha import AlephAlphaAPIModel  # noqa F401
+
 if is_extra_installed(extra="transformers"):
     from eval_framework.llm.huggingface import (  # noqa F401
         HFLLMRegistryModel,
@@ -14,8 +17,12 @@ if is_extra_installed(extra="transformers"):
         Smollm135MInstruct,
         Qwen3_0_6B,
     )
+
 if is_extra_installed("mistral"):
     from eval_framework.llm.mistral import MagistralVLLM  # noqa F401
+
+if is_extra_installed("openai"):
+    from eval_framework.llm.openai import OpenAIModel  # noqa F401
 
 if is_extra_installed("vllm"):
     from eval_framework.llm.vllm import VLLMRegistryModel, Qwen3_0_6B_VLLM, Qwen3_0_6B_VLLM_No_Thinking  # noqa F401
