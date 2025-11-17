7184fd271efc87fa456fe08cf06790d321d3f698
jordisassoon <jordisassoon@gmail.com>
2025-11-17 13:38:47 +0000

    mypy

--- src/eval_framework/utils/tqdm_handler.py
+++ src/eval_framework/utils/tqdm_handler.py
@@ -5,10 +5,10 @@ from tqdm import tqdm
 logger = logging.getLogger(__name__)
 
 
-def safe_tqdm_write(msg, level=logging.INFO):
+def safe_tqdm_write(msg: str, level: int = logging.INFO) -> None:
     if logger.isEnabledFor(level):
         tqdm.write(msg)
 
 
-def get_disable_bar_flag():
+def get_disable_bar_flag() -> bool:
     return logger.getEffectiveLevel() >= logging.WARNING
