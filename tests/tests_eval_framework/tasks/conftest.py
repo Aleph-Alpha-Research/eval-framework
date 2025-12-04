c998b8b9ffda35877c39fbf50ffd49d113b49093
ahmedhammam <ahmed.hammam@aleph-alpha-ip.ai>
2025-12-04 22:51:44 +0000

    skip wmt prompt ashing

--- tests/tests_eval_framework/tasks/conftest.py
+++
@@ -1,8 +0,0 @@
-import pytest
-
-
-def pytest_collection_modifyitems(items):
-    """Mark WMT tests to run serially to avoid file I/O race conditions."""
-    for item in items:
-        if "WMT" in item.nodeid:
-            item.add_marker(pytest.mark.xdist_group("wmt_serial"))
