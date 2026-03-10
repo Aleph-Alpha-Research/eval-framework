ef28f49226c34d5329c61b4c96cc776b6225a4a1
Prabhu Sivaprasad <prabhu.sivaprasad@aleph-alpha-research.com>
2026-03-10 09:42:20 +0000

    convert bytes to string

--- src/eval_framework/tasks/utils.py
+++ src/eval_framework/tasks/utils.py
@@ -110,6 +110,7 @@ def execute_python_code_with_tests(
 
     # Run the combined code in the sandbox
     output = run_python_code(combined_code, image=image, timeout=timeout, packages=packages)
+    output = output.decode("utf-8")
 
     # Parse the output to determine success
     return parse_output_fn(output)
