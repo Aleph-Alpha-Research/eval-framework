75ac40bddc73c76f48badcd4b48d819a3bb77f14
Markus Klein <markus.klein@aleph-alpha.com>
2026-08-28 10:09:06 +0200

    refactor: hellaswag_ellamind migrated to composed benchmark

--- src/eval_framework/tasks/task_names.py
+++ src/eval_framework/tasks/task_names.py
@@ -3,6 +3,7 @@ from enum import Enum
 from eval_framework.benchmarks.arc_de import ARC_DE_BENCHMARK
 from eval_framework.benchmarks.csqa_ellamind import CSQA_ELLAMIND_BENCHMARKS
 from eval_framework.benchmarks.gpqa_ellamind import GPQA_ELLAMIND_BENCHMARKS
+from eval_framework.benchmarks.hellaswag_ellamind import HELLASWAG_ELLAMIND_BENCHMARKS
 from eval_framework.benchmarks.piqa_ellamind import PIQA_ELLAMIND_BENCHMARKS
 from eval_framework.benchmarks.simpleqa_ellamind import SIMPLEQA_ELLAMIND_BENCHMARKS
 from eval_framework.benchmarks.siqa_ellamind import SIQA_ELLAMIND_BENCHMARKS
@@ -263,15 +264,8 @@ def register_gsm8k_ellamind_tasks(registry: Registry) -> None:
 
 def register_hellaswag_ellamind_tasks(registry: Registry) -> None:
     """Register hellaswag_ellamind benchmark tasks."""
-    register_lazy_task(
-        "eval_framework.tasks.benchmarks.hellaswag_ellamind.HELLASWAG_ELLAMIND_EASY_DE", registry=registry
-    )
-    register_lazy_task(
-        "eval_framework.tasks.benchmarks.hellaswag_ellamind.HELLASWAG_ELLAMIND_HARD_DE", registry=registry
-    )
-    register_lazy_task(
-        "eval_framework.tasks.benchmarks.hellaswag_ellamind.HELLASWAG_ELLAMIND_BPB_DE", registry=registry
-    )
+    for benchmark in HELLASWAG_ELLAMIND_BENCHMARKS:
+        registry.add(benchmark)
 
 
 def register_hendrycks_math_ellamind_tasks(registry: Registry) -> None:
